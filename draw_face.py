52b9fde5c41be58fd8210be9b4d8a21622d42b12
Thenea <thenea@iMac4.local>
2018-12-07 08:57:41 -0500

    just started my project, created the self file

---
+++ draw_face.py
@@ -0,0 +1,18 @@
+import pygame
+
+
+class draw_face:
+
+    def __init__(self, main_surface):
+        self.main_surface = main_surface
+        self.GREEN = (0, 102, 71)
+        self.GOLD = (255, 209, 63)
+        self.WHITE = (255, 255, 255)
+        self.BLACK = (0, 0, 0)
+        self.WHITE = (255, 255, 255)
+        self.PINK = (255, 192, 203)
+        self.RED = (255, 0, 0)
+        self.BLUE = (0, 0, 255)
+
+    def draw_circle(self):
+        pygame.draw.
